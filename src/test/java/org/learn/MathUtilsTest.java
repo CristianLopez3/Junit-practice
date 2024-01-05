812012579b4488600dbbefdeebf32dd092a35d40
cristian <cristian.c.lopez.m@gmail.com>
2024-01-04 20:58:52 -0500

    TestInfo and TestReporter for depure or debuggin  your code

--- src/test/java/org/learn/MathUtilsTest.java
+++ src/test/java/org/learn/MathUtilsTest.java
@@ -5,20 +5,28 @@ import org.junit.jupiter.api.condition.EnabledOnOs;
 import org.junit.jupiter.api.condition.OS;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assumptions.*;
+import static org.junit.jupiter.api.Tag.*;
+
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @DisplayName("When the start Math Utils Test Class Run")
 public class MathUtilsTest {
 
     MathUtils mathUtils;
+    TestInfo testInfo;
+    TestReporter testReporter;
 
 
     @BeforeEach // Allow this method run before each method.
-    void init(){
+    @Tag("Math")
+    void init(TestInfo testInfo, TestReporter testReporter){
+        this.testInfo = testInfo;
+        this.testReporter = testReporter;
         mathUtils = new MathUtils();
     }
 
     @Nested
     @DisplayName("Testing add method")
+    @Tag("Math")
     class AddTest {
         @Test
         @DisplayName("when  add method is testing two positive numbers ")
@@ -38,12 +46,13 @@ public class MathUtilsTest {
 
     }
 
-
-
     @Test
     @DisplayName("Multiply method")
+    @Tag("Math")
     void testMultiply(){
 //        assertEquals(-1, mathUtils.multiply(-1, -1), "Should return the right product");
+//        System.out.println("Running "+testInfo.getDisplayName()+" with tags "+testInfo.getTags());
+        testReporter.publishEntry("Running "+testInfo.getDisplayName()+" with tags "+testInfo.getTags());
         assertAll(
                 () -> assertEquals(1, mathUtils.multiply(-1, -1), "Should return the right product"),
                 () -> assertEquals(0, mathUtils.multiply(2, 0), "Should return the right product"),
@@ -55,6 +64,7 @@ public class MathUtilsTest {
 //    @EnabledOnOs(OS.LINUX)
     @Test
     @DisplayName("Testing divide method")
+    @Tag("Math")
     void testDivide(){
         assumeTrue(true); // Is like a condition will stop the method if is fail the assumption.
         assertThrows(
@@ -65,8 +75,11 @@ public class MathUtilsTest {
 
 
 
-    @RepeatedTest(3)
-    void testComputeCircleArea(RepetitionInfo repetitionInfo){
+//    @RepeatedTest(3)
+    @Tag("Math")
+    @Test
+    void testComputeCircleArea(){
+//        RepetitionInfo
         assertEquals(314.1592653589793, mathUtils.computeCircleArea(10), "The compute circle area should find the area of the radios giving.");
     }
 
